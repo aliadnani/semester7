d42c249c4cd23886c8f396d18aa6ed1f258979f4
aliadnani <ali.adnani@hotmail.com>
2020-09-08 18:32:32 +0800

    Deploy website - based on 160f0a28a0277d5020bf36caf9000d193b010721

--- af84587b.24a70844.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{82:function(e){e.exports=JSON.parse('{"permalink":"/semester7/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog"}')}}]);
